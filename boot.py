18cc712b66e31d6613133449333e7839e68fa993
Lamarqe <scritch@gmx.de>
2026-01-17 21:35:19 +0100

    initial network setup

--- boot.py
+++ boot.py
@@ -1,8 +1,13 @@
+import json
+
 import machine
 import network
 import webrepl
+from network import LAN, WLAN
 
-lan = network.LAN(
+nw_config = json.load(open("config/network.json"))
+
+lan = LAN(
     mdc=machine.Pin(23),
     mdio=machine.Pin(18),
     power=machine.Pin(12),
@@ -12,6 +17,18 @@ lan = network.LAN(
     ref_clk_mode=machine.Pin.OUT,
 )
 lan.active(True)
-lan.ipconfig("addr4")
+lan.ipconfig(addr4=nw_config["ipconfig"]["addr4"], gw4=nw_config["ipconfig"]["gw4"])
+if not lan.isconnected():
+    lan.active(False)
+    wlan = WLAN(network.STA_IF)
+    wlan.active(True)
+    network.ipconfig(dns=nw_config["ipconfig"]["dns"])
+    wlan.ipconfig(addr4=nw_config["ipconfig"]["addr4"], gw4=nw_config["ipconfig"]["gw4"])
+    if not wlan.isconnected():
+        print("connecting to network...")
+        wlan.connect(nw_config["wifi"]["ssid"], nw_config["wifi"]["password"])
+        while not wlan.isconnected():
+            pass
+    print("network config:", wlan.ifconfig())
 
-webrepl.start()
+webrepl.start(password="1234")
